b167a879c2e5b6318d92ead7e1e1d46e9240bf3c
ibrahim-hmood <ibrahim.hmood99@gmail.com>
2025-02-07 01:36:05 -0600

    switching to flask, adding mobile support

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#Checks if device is mobile
+@app.route("/is_mobile")
+def is_mobile():
+    devices = ["iphone", "android", "blackberry"]
+    agent = request.headers.get('User-Agent').lower()
+    for device in devices:
+        if(device in agent):
+            return True
+    return False
+
+
+#The mobile version of our interface
+@app.route("/mobile")
+def mobile():
+    #Load the mobile version
+    return render_template("mobile.html")
+
+#The main page, desktop mode
+@app.route("/")
+def index():
+    #Check if device is mobile
+    print(is_mobile())
+    if(is_mobile()):
+        return mobile()
+    #Just render main index
+    return render_template("index.html")
+
+if(__name__ == "__main__"):
+    app.run(debug = True)
